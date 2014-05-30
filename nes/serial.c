20a7dbcff164c43cf153ea1f8e42b44e2866fcab
Damián Silvani <munshkr@gmail.com>
2014-05-30 20:48:20 -0300

    nes: Print value after polling joystick controller

--- nes/serial.c
+++ nes/serial.c
@@ -3,14 +3,28 @@
 
 #include "neslib.h"
 
-
-// macro to calculate nametable address from X,Y in compile time
-
 #define NTADR(x,y) ((NAMETABLE_A|((y)<<5)|x))
 
+#define MSB(x)        (((x)>>8))
+#define LSB(x)        (((x)&0xff))
+
+// variables
+static unsigned char val;
+
+// the update list, it is for 6 tiles, 3 bytes per tile
+static unsigned char list[6*3+1];
+
+// init data for the update list, it contains MSB and LSB of a tile address
+// in the nametable, then the tile number
+const unsigned char list_init[6*3+1]={
+    MSB(NTADR(9,4)), LSB(NTADR(9,4)), 0, // non-sequental updates
+    MSB(NTADR(10,4)), LSB(NTADR(10,4)), 0,
+    MSB(NTADR(11,4)), LSB(NTADR(11,4)), 0,
+
+    NT_UPD_EOF
+};
 
 // put a string into the nametable
-
 void put_str(unsigned int adr,const char *str)
 {
     vram_adr(adr);
@@ -23,23 +37,31 @@ void put_str(unsigned int adr,const char *str)
 }
 
 
-
 void main(void)
 {
-    // rendering is disabled at the startup, the palette is all black
+    pal_col(1, 0x30); // white
 
-    pal_col(1, 0x30); // set while color
+    // initialize update list
+    memcpy(list, list_init, sizeof(list_init));
 
-    // you can't put data into vram through vram_put while rendering is enabled
-    // so you have to disable rendering to put things like text or a level map
-    // into the nametable
+    set_vram_update(list);
 
-    // there is a way to update small number of nametable tiles while rendering
-    // is enabled, using set_vram_update and an update list
-
-    put_str(NTADR(2, 2),"HELLO, WORLD!");
+    put_str(NTADR(2, 2), "NES-SERIAL");
+    put_str(NTADR(2, 4), "VALUE: ");
 
     ppu_on_all(); // enable rendering
 
-    while(1); // do nothing, infinite loop
+    while(1)
+    {
+        ppu_wait_frame(); // wait for next TV frame
+
+        val = pad_poll(0);  // poll joystick 0, i contains 8bit value
+
+        if (val > 0) {
+            // change update list to reflect new value
+            list[2]= val / 100     + 0x10;
+            list[5]= val / 10 % 10 + 0x10;
+            list[8]= val      % 10 + 0x10;
+        }
+    }
 }
