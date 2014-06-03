458d358bb07d6cc06ef8eb65df9085b970e7e328
Damián Silvani <munshkr@gmail.com>
2014-06-02 22:49:05 -0300

    Easier aproach: use a 4021 8bit shifter (same one used in joystick)
    This works! \^o^/

--- ino/src/sketch.ino
+++ ino/src/sketch.ino
@@ -1,38 +1,34 @@
 // NES serial communication example
 // Sends an incrementing value every 0.5s to the NES
-//
-// @munshkr (munshkr@gmail.com) / 2014-05-30
 
-                                // NES color pinout:
-const uint8_t LATCH_PIN = 3;    //  orange
-const uint8_t DATA_PIN  = 4;    //  yellow
-const uint8_t CLOCK_PIN = 5;    //  red
-                                //  brown = GND
 const uint8_t LED_PIN = 13;
+const uint8_t INPUT_PINS[8] = { 9, 8, 7, 6, 5, 4, 3, 2 };
 
-volatile uint8_t value;
+uint8_t value;
 
 void setup()
 {
-    pinMode(LATCH_PIN, INPUT_PULLUP);
-    pinMode(CLOCK_PIN, INPUT_PULLUP);
-    pinMode(DATA_PIN, OUTPUT);
-
-    digitalWrite(DATA_PIN, LOW);
-
     // enable led, turn off
     pinMode(LED_PIN, OUTPUT);
     digitalWrite(LED_PIN, LOW);
 
+    for (int i = 0; i < 8; i++) {
+        pinMode(INPUT_PINS[i], OUTPUT);
+        digitalWrite(INPUT_PINS[i], HIGH);
+    }
+
     value = 0;
 
-    attachInterrupt(1, latchISR, RISING);
+    Serial.begin(9600);
 }
 
 void loop()
 {
+    delay(100);
+
     value++;
-    delay(1000);
+    sendValue(value);
+    Serial.println(value);
 
     // flash led
     digitalWrite(LED_PIN, HIGH);
@@ -40,36 +36,11 @@ void loop()
     digitalWrite(LED_PIN, LOW);
 }
 
-// This ISR must be attached to the latch pin on a RISING edge.
-// Check NES controller data flow for reference.
-//
-void latchISR()
+// Sends value in bits using the 8 output pins
+void sendValue(uint8_t value)
 {
-    shiftOutWithClk(DATA_PIN, CLOCK_PIN, LSBFIRST, value);
-}
-
-// Same as shiftOut, but uses the clockPin as an external clock,
-// and writes to dataPin after each rising edge of that clock.
-//
-void shiftOutWithClk(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val)
-{
-    uint8_t i;
-    uint8_t vb;
-
-    for (i = 0; i < 8; i++)  {
-        if (bitOrder == LSBFIRST) {
-            vb = !!(val & (1 << i));
-        } else {
-            vb = !!(val & (1 << (7 - i)));
-        }
-
-        // write the inverted value (NES inverts it)
-        digitalWrite(dataPin, !vb);
-
-        // wait for a full cycle of the clock
-        while (digitalRead(clockPin) == HIGH);
-        while (digitalRead(clockPin) == LOW);
-    }
-
-    digitalWrite(dataPin, LOW);
+    for (int i = 7; i >= 0; i--) {
+        int res = value >> i;
+        digitalWrite(INPUT_PINS[i], !(res & 1));
+    }  
 }
